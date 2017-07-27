b7781e3600629aded394c2a5a2e8fdc8b3dee894
Mónica Prados <monpragodev@gmail.com>
2017-07-27 13:22:27 +0200

    - Añadidas dependencias con Composer. - Eliminada carpeta "apps" (ahora se usa la carpeta vendor). - Cambios en index.

--- index.php
+++ index.php
@@ -2,14 +2,47 @@
 <!--
 Author : Mónica Prados Gómez
 -->
-<html>
+<html lang="es">
     <head>
         <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Cat-a-log</title>
+        <!-- Bootstrap -->
+        <link rel="stylesheet" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
     </head>
     <body>
-        <?php
-        // put your code here
-        ?>
+        <nav class="navbar navbar-inverse navbar-fixed-top">
+            <div class="container">
+                <div class="navbar-header">
+                  <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                  </button>
+                  <a class="navbar-brand" href="#">Cat-a-log</a>
+                </div>
+                <div id="navbar" class="navbar-collapse collapse">
+                  <form class="navbar-form navbar-right">
+                    <button type="button" name="login" class="btn btn-success">Inicia sesión</button>
+                    <button type="button" name="register" class="btn btn-primary">Regístrate</button>
+                  </form>
+                </div>
+            </div>
+        </nav>
+        <div class="jumbotron text-center">
+          <img src="img/logoviejo.png" alt="Cat-a-log logo" class="">
+          <h1>Cat-a-log</h1>
+          <h2>¡Digitaliza tu colección de videojuegos físicos!</h2>
+        </div>
+        <div class="container text-center">
+          <p>¿Eres de los que compra juegos físicos porque las cajas quedan muy bien en la estantería, pero te gustaría fardar de dichos juegos también online?</p>
+          <p>Cat-a-log te permite registrar todos los juegos que posees para mostrarlos en un perfil digital, al alcance de todo el mundo.</p>
+          <p>Entra y descúbrelo.</p>
+        </div>
+        <!-- jQuery -->
+        <script src="vendor/components/jquery/jquery.min.js"></script>
+        <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
     </body>
 </html>
